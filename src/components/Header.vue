c02f1e2d45631a0b250bfd3fccb19449dba0f67d
pavry <43524127+pavry@users.noreply.github.com>
2025-07-10 12:01:34 +0000

    Deploying to dev from @ pavry/pavry.github.io@59c4fae03434f6b7a3eff8b8703cee04c7f499df 🚀

--- src/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="space-y-2">
-  <img src="/images/keyboard-maniac.gif" style="display: block; margin: 0 auto; width: 100%; height:250px;"/>
-  <div class="flex items-center gap-2 text-sm text-gruvbox-gray">
-    <font-awesome-icon :icon="['fas', 'code']" class="w-4 h-4 mr-0.5" />
-    <div>Ctrl+C, Ctrl+V engineer.</div>
-  </div>
-  </div>
-</template>
